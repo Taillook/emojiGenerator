7e42c6278b7c829fa0c0063279aa97d215a6870e
Taillook <jousoku@gmail.com>
2017-04-29 15:40:20 +0900

    first commit

---
+++ emojiGenerator.py
@@ -0,0 +1,19 @@
+# -*- encoding:utf8 -*-
+
+from PIL import Image, ImageDraw, ImageFont
+import sys
+
+def emoji_generate(text):
+    #TureTypeFont and FontSize
+    font = ImageFont.truetype("/Users/taillook/Library/RictyDiminished-Bold.ttf", 64)
+
+    image = Image.new('RGB', (128, 128), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+
+    #(xPoint, yPoint) String FontColor
+    draw.text((0, 0), text[0:3] + "\n" + text[2:5], font = font, fill='#000000')
+
+    image.save('emoji.png', 'PNG')
+
+emoji_generate(sys.argv[1])
+
