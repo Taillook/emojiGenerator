ddca3d6f31460dc3dd681caa60ae98adfa4bb428
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-05-03 15:51:47 +0900

    add execution script

---
+++ generate_emoji.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+
+from emoji_generator.generate import generate_emoji
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("character", type=str, help="target character")
+    parser.add_argument("--font", default="Ricty Bold.ttf")
+    args = parser.parse_args()
+
+    generate_emoji(args.character, args.font)
