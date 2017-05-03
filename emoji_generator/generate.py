771be57252624bfedc768a6c14f5b9fb8f9df2bb
Taillook <jousoku@gmail.com>
2017-05-03 20:09:55 +0900

    fix slice length

--- emoji_generator/generate.py
+++ emoji_generator/generate.py
@@ -13,5 +13,5 @@ def generate_emoji(text, fonttype="Ricty-Bold.ttf", fontsize=64):
     image = Image.new("RGB", (128, 128), (255, 255, 255))
     draw = ImageDraw.Draw(image)
 
-    draw.text((0, 0), "{}\n{}".format(text[0:3], text[2:5]), font=font, fill="#000000")
+    draw.text((0, 0), "{}\n{}".format(text[0:3], text[2:4]), font=font, fill="#000000")
     image.save("emoji_{}.png".format(text), "PNG")
--- generate_emoji.py
+++ generate_emoji.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("character", type=str, help="target character")
-    parser.add_argument("--font", default="Ricty Bold.ttf")
+    parser.add_argument("--font", default="Ricty-Bold.ttf")
     args = parser.parse_args()
 
     generate_emoji(args.character, args.font)
