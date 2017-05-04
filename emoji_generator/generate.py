09ad6569808b7926a22b9210a0425cf82333c6e1
Taillook <jousoku@gmail.com>
2017-05-04 22:56:40 +0900

    [Add] can choose color

--- emoji_generator/generate.py
+++ emoji_generator/generate.py
@@ -6,12 +6,12 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 
-def generate_emoji(text, fonttype="Ricty-Bold.ttf", fontsize=64):
+def generate_emoji(text, color, fonttype="Ricty-Bold.ttf", fontsize=64):
 
     # Specify TrueType font and Fontsize
     font = ImageFont.truetype(fonttype, fontsize)
     image = Image.new("RGB", (128, 128), (255, 255, 255))
     draw = ImageDraw.Draw(image)
 
-    draw.text((0, 0), "{}\n{}".format(text[0:3], text[2:4]), font=font, fill="#000000")
+    draw.text((0, 0), "{}\n{}".format(text[0:3], text[2:4]), font=font, fill=color)
     image.save("emoji_{}.png".format(text), "PNG")
--- generate_emoji.py
+++ generate_emoji.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("character", type=str, help="target character")
-    parser.add_argument("--font", default="Ricty-Bold.ttf")
+    parser.add_argument("--font", type=str, default="Ricty-Bold.ttf")
+    parser.add_argument("--color", type=str, default="#000000")
     args = parser.parse_args()
-
-    generate_emoji(args.character, args.font)
+    print(args)
+    generate_emoji(args.character, args.color, args.font)
